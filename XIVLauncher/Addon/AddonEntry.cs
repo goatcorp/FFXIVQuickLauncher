6c13d4d45f72060343144b07f81b1583bc7e5b55
Roy <royroy.euph@gmail.com>
2019-06-23 02:27:14 +0900

    Add start timing to AddonEntry Class.

--- XIVLauncher/Addon/AddonEntry.cs
+++ XIVLauncher/Addon/AddonEntry.cs
@@ -1,8 +1,15 @@
 ﻿namespace XIVLauncher.Addon
 {
+    public enum AddonStartAt
+    {
+        LauncherInitialised,
+        GameLaunched,
+    }
+
     public class AddonEntry
     {
         public bool IsEnabled { get; set; }
+        public AddonStartAt StartAt { get; set; } = AddonStartAt.GameLaunched;
         public IAddon Addon { get; set; }
     }
 }
