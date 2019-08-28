ebd033f3861feb4165260ce338782010124c417b
goaaats <16760685+goaaats@users.noreply.github.com>
2019-08-28 14:44:37 +0900

    New translations GenericAddonSetupWindow.xaml.cs (Japanese)

---
+++ translated/ja/GenericAddonSetupWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using XIVLauncher.Addon;
+
+namespace XIVLauncher.Windows
+{
+    /// <summary>
+    ///     Interaction logic for FirstTimeSetup.xaml
+    /// </summary>
+    public partial class GenericAddonSetupWindow : Window
+    {
+        public GenericAddon Result { get; private set; }
+
+        public GenericAddonSetupWindow(GenericAddon addon = null)
+        {
+            InitializeComponent();
+
+            if (addon != null)
+            {
+                PathEntry.Text = addon.Path;
+                CommandLineTextBox.Text = addon.CommandLine;
+                AdminCheckBox.IsChecked = addon.RunAsAdmin;
+                KillCheckBox.IsChecked = addon.KillAfterClose;
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(PathEntry.Text))
+                Close();
+
+            Result = new GenericAddon
+            {
+                Path = PathEntry.Text,
+                CommandLine = CommandLineTextBox.Text,
+                RunAsAdmin = AdminCheckBox.IsChecked == true,
+                KillAfterClose = KillCheckBox.IsChecked == true
+            };
+
+            Close();
+        }
+
+        private void AdminCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            KillCheckBox.IsEnabled = false;
+            KillCheckBox.IsChecked = false;
+        }
+
+        private void AdminCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            KillCheckBox.IsEnabled = true;
+        }
+    }
+}
