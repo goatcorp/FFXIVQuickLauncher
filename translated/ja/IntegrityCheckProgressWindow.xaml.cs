78b0340d0d175a1557f758015a3c7f2a3d3df43f
goaaats <16760685+goaaats@users.noreply.github.com>
2019-08-28 14:44:39 +0900

    New translations IntegrityCheckProgressWindow.xaml.cs (Japanese)

---
+++ translated/ja/IntegrityCheckProgressWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using XIVLauncher.Game;
+
+namespace XIVLauncher.Windows
+{
+    /// <summary>
+    ///     Interaction logic for FirstTimeSetup.xaml
+    /// </summary>
+    public partial class IntegrityCheckProgressWindow : Window
+    {
+        public IntegrityCheckProgressWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateProgress(IntegrityCheck.IntegrityCheckProgress progress)
+        {
+            InfoTextBlock.Text = $"{progress.CurrentFile}";
+        }
+    }
+}
