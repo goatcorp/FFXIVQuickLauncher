ae87d0c3c355ebc2e61725274d20be47208ad775
goat <fakecake@icloud.com>
2019-12-29 12:48:16 +0900

    build: v4.5.0.0

--- XIVLauncher/Properties/AssemblyInfo.cs
+++ XIVLauncher/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.4.0.0")]
-[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
