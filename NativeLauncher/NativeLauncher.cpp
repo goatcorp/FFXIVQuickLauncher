56b13816b54de475ee97d469268de5fd57cf6be0
goat <fakecake@icloud.com>
2020-02-23 02:53:05 +0900

    fix: use melis method for patching ACL crash

--- NativeLauncher/NativeLauncher.cpp
+++ NativeLauncher/NativeLauncher.cpp
@@ -4,6 +4,196 @@
 #include <windows.h>
 #include <AccCtrl.h>
 #include <AclAPI.h>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+struct HEX
+{
+   HEX(unsigned long num, unsigned long fieldwidth = 8, bool bUpcase = false)
+      : m_num(num), m_width(fieldwidth), m_upcase(bUpcase)
+   {}
+
+   unsigned long m_num;
+   unsigned long m_width;
+   bool m_upcase;
+};
+
+inline ostream& operator << (ostream& os, const HEX& h)
+{
+   int fmt = os.flags();
+   char fillchar = os.fill('0');
+   os << "0x" << hex << (h.m_upcase ? uppercase : nouppercase) << setw(h.m_width) << h.m_num;
+   os.fill(fillchar);
+   os.flags(fmt);
+   return os;
+}
+
+inline wostream& operator << (wostream& os, const HEX& h)
+{
+   int fmt = os.flags();
+   wchar_t fillchar = os.fill(L'0');
+   os << L"0x" << hex << (h.m_upcase ? uppercase : nouppercase) << setw(h.m_width) << h.m_num;
+   os.fill(fillchar);
+   os.flags(fmt);
+   return os;
+}
+
+inline std::string SysErrorMessageWithCode(DWORD dwErrCode /*= GetLastError()*/)
+{
+   LPWSTR pszErrMsg = NULL;
+   std::stringstream sRetval;
+   DWORD flags =
+      FORMAT_MESSAGE_ALLOCATE_BUFFER |
+      FORMAT_MESSAGE_IGNORE_INSERTS |
+      FORMAT_MESSAGE_FROM_SYSTEM;
+
+   if (FormatMessageW(
+      flags,
+      NULL,
+      dwErrCode,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+      (LPWSTR)&pszErrMsg,
+      0,
+      NULL))
+   {
+      sRetval << pszErrMsg << L" (Error # " << dwErrCode << L" = " << HEX(dwErrCode) << L")";
+      LocalFree(pszErrMsg);
+   }
+   else
+   {
+      sRetval << L"Error # " << dwErrCode << L" (" << HEX(dwErrCode) << L")";
+   }
+   return sRetval.str();
+}
+
+bool RunAsDesktopUser(
+   __in    const wchar_t* szApp,
+   __in    wchar_t* szCmdLine,
+   __in    const wchar_t* szCurrDir,
+   __in    LPSECURITY_ATTRIBUTES pSec,
+   __in    LPSTARTUPINFOW si,
+   __inout LPPROCESS_INFORMATION pi)
+{
+   HANDLE hShellProcess = NULL, hShellProcessToken = NULL, hPrimaryToken = NULL;
+   HWND hwnd = NULL;
+   DWORD dwPID = 0;
+   BOOL ret;
+   DWORD dwLastErr;
+
+   // Enable SeIncreaseQuotaPrivilege in this process.  (This won't work if current process is not elevated.)
+   HANDLE hProcessToken = NULL;
+   if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hProcessToken))
+   {
+      dwLastErr = GetLastError();
+      cout << L"OpenProcessToken failed:  " << SysErrorMessageWithCode(dwLastErr);
+      return false;
+   }
+   else
+   {
+      TOKEN_PRIVILEGES tkp;
+      tkp.PrivilegeCount = 1;
+      LookupPrivilegeValueW(NULL, SE_INCREASE_QUOTA_NAME, &tkp.Privileges[0].Luid);
+      tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+      AdjustTokenPrivileges(hProcessToken, FALSE, &tkp, 0, NULL, NULL);
+      dwLastErr = GetLastError();
+      CloseHandle(hProcessToken);
+      if (ERROR_SUCCESS != dwLastErr)
+      {
+         cout << L"AdjustTokenPrivileges failed:  " << SysErrorMessageWithCode(dwLastErr);
+         return false;
+      }
+   }
+
+   // Get an HWND representing the desktop shell.
+   // CAVEATS:  This will fail if the shell is not running (crashed or terminated), or the default shell has been
+   // replaced with a custom shell.  This also won't return what you probably want if Explorer has been terminated and
+   // restarted elevated.
+   hwnd = GetShellWindow();
+   if (NULL == hwnd)
+   {
+      cout << L"No desktop shell is present";
+      return false;
+   }
+
+   // Get the PID of the desktop shell process.
+   GetWindowThreadProcessId(hwnd, &dwPID);
+   if (0 == dwPID)
+   {
+      cout << L"Unable to get PID of desktop shell.";
+      return false;
+   }
+
+   // Open the desktop shell process in order to query it (get the token)
+   hShellProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwPID);
+   if (!hShellProcess)
+   {
+      dwLastErr = GetLastError();
+      cout << L"Can't open desktop shell process:  " << SysErrorMessageWithCode(dwLastErr);
+      return false;
+   }
+
+   // Get the process token of the desktop shell.
+   ret = OpenProcessToken(hShellProcess, TOKEN_DUPLICATE, &hShellProcessToken);
+   if (!ret)
+   {
+      dwLastErr = GetLastError();
+      cout << L"Can't get process token of desktop shell:  " << SysErrorMessageWithCode(dwLastErr);
+      return false;
+   }
+
+   // Duplicate the shell's process token to get a primary token.
+   // Based on experimentation, this is the minimal set of rights required for CreateProcessWithTokenW (contrary to current documentation).
+   const DWORD dwTokenRights = TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID;
+   ret = DuplicateTokenEx(hShellProcessToken, dwTokenRights, NULL, SecurityImpersonation, TokenPrimary, &hPrimaryToken);
+   if (!ret)
+   {
+      dwLastErr = GetLastError();
+      cout << L"Can't get primary token:  " << SysErrorMessageWithCode(dwLastErr);
+      return false;
+   }
+
+   // Start the target process with the new token.
+   ret = CreateProcessAsUserW(
+      hPrimaryToken,
+      szApp,
+      szCmdLine,
+      pSec,
+      nullptr,
+      0,
+      0,
+      NULL,
+      szCurrDir,
+      si,
+      pi);
+   if (!ret)
+   {
+      dwLastErr = GetLastError();
+      cout << L"CreateProcessAsUserW failed:  " << SysErrorMessageWithCode(dwLastErr);
+      return false;
+   }
+
+   return true;
+}
+
+
+
+BOOL IsElevated() {
+   BOOL fRet = FALSE;
+   HANDLE hToken = NULL;
+   if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+      TOKEN_ELEVATION Elevation;
+      DWORD cbSize = sizeof(TOKEN_ELEVATION);
+      if (GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
+         fRet = Elevation.TokenIsElevated;
+      }
+   }
+   if (hToken) {
+      CloseHandle(hToken);
+   }
+   return fRet;
+}
 
 struct handle_data {
    unsigned long process_id;
@@ -50,7 +240,8 @@ int launch_game(char* appC, char* argC)
    DWORD size = 256;
    if (!GetUserName((TCHAR*)username, &size))
    {
-      return GetLastError();
+      std::cout << "GetUserName failed";
+      return -1;
    }
 
    EXPLICIT_ACCESS pExplicitAccess;
@@ -64,12 +255,14 @@ int launch_game(char* appC, char* argC)
    ZeroMemory(&secDes, sizeof(secDes));
    if (!InitializeSecurityDescriptor(&secDes, 1u))
    {
-      return GetLastError();
+      std::cout << "InitializeSecurityDescriptor failed";
+      return -1;
    }
 
    if (!SetSecurityDescriptorDacl(&secDes, true, NewAcl, false))
    {
-      return GetLastError();
+      std::cout << "SetSecurityDescriptorDacl failed";
+      return -1;
    }
 
    STARTUPINFO si;
@@ -85,10 +278,12 @@ int launch_game(char* appC, char* argC)
    pSec.lpSecurityDescriptor = &secDes;
    pSec.bInheritHandle = false;
 
-   if (!CreateProcess(app_const, arg_concat, &pSec, nullptr, false, 0, nullptr, nullptr, &si, &pi))
+   if (!CreateProcess(nullptr, arg_concat, &pSec, nullptr, false, 0x20, nullptr, nullptr, &si, &pi))
    {
-      return GetLastError();
+      std::cout << "CreateProcess failed";
+      return -1;
    }
+   
 
    while (!has_window(pi.dwProcessId))
    {
@@ -96,8 +291,22 @@ int launch_game(char* appC, char* argC)
    }
 
    PACL myAcl;
-   GetSecurityInfo(GetCurrentProcess(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, &myAcl, nullptr, nullptr);
+   auto gsi = GetSecurityInfo(GetCurrentProcess(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, &myAcl, nullptr, nullptr);
+   if (gsi != ERROR_SUCCESS)
+   {
+      std::cout << "GetSecurityInfo failed";
+      return -1;
+   }
+
    auto ssi = SetSecurityInfo(pi.hProcess, SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION | UNPROTECTED_DACL_SECURITY_INFORMATION, nullptr, nullptr, myAcl, nullptr);
+   if (ssi != ERROR_SUCCESS)
+   {
+      std::cout << "SetSecurityInfo failed";
+      return -1;
+   }
+
+   CloseHandle(pi.hProcess);
+   CloseHandle(pi.hThread);
 
    return pi.dwProcessId;
 }
---
+++ XIVLauncher/Game/NativeGameStart.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVLauncher.Game
+{
+    static class NativeGameStart
+    {
+        #region DllImports
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetThreadContext(IntPtr hThread, ref CONTEXT64 lpContext);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine, 
+            IntPtr lpProcessAttributes, 
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            CreateProcessFlags dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            Int32 nSize,
+            out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        #endregion
+
+        #region Enums
+
+        public enum CONTEXT_FLAGS : uint
+        {
+            CONTEXT_i386 = 0x10000,
+            CONTEXT_i486 = 0x10000,   //  same as i386
+            CONTEXT_CONTROL = CONTEXT_i386 | 0x01, // SS:SP, CS:IP, FLAGS, BP
+            CONTEXT_INTEGER = CONTEXT_i386 | 0x02, // AX, BX, CX, DX, SI, DI
+            CONTEXT_SEGMENTS = CONTEXT_i386 | 0x04, // DS, ES, FS, GS
+            CONTEXT_FLOATING_POINT = CONTEXT_i386 | 0x08, // 387 state
+            CONTEXT_DEBUG_REGISTERS = CONTEXT_i386 | 0x10, // DB 0-3,6,7
+            CONTEXT_EXTENDED_REGISTERS = CONTEXT_i386 | 0x20, // cpu specific extensions
+            CONTEXT_FULL = CONTEXT_CONTROL | CONTEXT_INTEGER | CONTEXT_SEGMENTS,
+            CONTEXT_ALL = CONTEXT_CONTROL | CONTEXT_INTEGER | CONTEXT_SEGMENTS | CONTEXT_FLOATING_POINT | CONTEXT_DEBUG_REGISTERS | CONTEXT_EXTENDED_REGISTERS
+        }
+
+        [Flags]
+        enum CreateProcessFlags
+        {
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_NO_WINDOW = 0x08000000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            DEBUG_PROCESS = 0x00000001,
+            DETACHED_PROCESS = 0x00000008,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            INHERIT_PARENT_AFFINITY = 0x00010000
+        }
+
+        #endregion
+
+        #region Structs
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        // This also works with CharSet.Ansi as long as the calling function uses the same character set.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFOEX
+        {
+            public STARTUPINFO StartupInfo;
+            public IntPtr lpAttributeList;
+        }
+
+        // This also works with CharSet.Ansi as long as the calling function uses the same character set.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLOATING_SAVE_AREA
+        {
+            public uint ControlWord;
+            public uint StatusWord;
+            public uint TagWord;
+            public uint ErrorOffset;
+            public uint ErrorSelector;
+            public uint DataOffset;
+            public uint DataSelector;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 80)]
+            public byte[] RegisterArea;
+            public uint Cr0NpxState;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CONTEXT
+        {
+            public uint ContextFlags; //set this to an appropriate value
+            // Retrieved by CONTEXT_DEBUG_REGISTERS
+            public uint Dr0;
+            public uint Dr1;
+            public uint Dr2;
+            public uint Dr3;
+            public uint Dr6;
+            public uint Dr7;
+            // Retrieved by CONTEXT_FLOATING_POINT
+            public FLOATING_SAVE_AREA FloatSave;
+            // Retrieved by CONTEXT_SEGMENTS
+            public uint SegGs;
+            public uint SegFs;
+            public uint SegEs;
+            public uint SegDs;
+            // Retrieved by CONTEXT_INTEGER
+            public uint Edi;
+            public uint Esi;
+            public uint Ebx;
+            public uint Edx;
+            public uint Ecx;
+            public uint Eax;
+            // Retrieved by CONTEXT_CONTROL
+            public uint Ebp;
+            public uint Eip;
+            public uint SegCs;
+            public uint EFlags;
+            public uint Esp;
+            public uint SegSs;
+            // Retrieved by CONTEXT_EXTENDED_REGISTERS
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+            public byte[] ExtendedRegisters;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct M128A
+        {
+            public ulong High;
+            public long Low;
+
+            public override string ToString()
+            {
+                return string.Format("High:{0}, Low:{1}", this.High, this.Low);
+            }
+        }
+
+        /// <summary>
+        /// x64
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 16)]
+        public struct XSAVE_FORMAT64
+        {
+            public ushort ControlWord;
+            public ushort StatusWord;
+            public byte TagWord;
+            public byte Reserved1;
+            public ushort ErrorOpcode;
+            public uint ErrorOffset;
+            public ushort ErrorSelector;
+            public ushort Reserved2;
+            public uint DataOffset;
+            public ushort DataSelector;
+            public ushort Reserved3;
+            public uint MxCsr;
+            public uint MxCsr_Mask;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public M128A[] FloatRegisters;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            public M128A[] XmmRegisters;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 96)]
+            public byte[] Reserved4;
+        }
+
+        /// <summary>
+        /// x64
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 16)]
+        public struct CONTEXT64
+        {
+            public ulong P1Home;
+            public ulong P2Home;
+            public ulong P3Home;
+            public ulong P4Home;
+            public ulong P5Home;
+            public ulong P6Home;
+
+            public CONTEXT_FLAGS ContextFlags;
+            public uint MxCsr;
+
+            public ushort SegCs;
+            public ushort SegDs;
+            public ushort SegEs;
+            public ushort SegFs;
+            public ushort SegGs;
+            public ushort SegSs;
+            public uint EFlags;
+
+            public ulong Dr0;
+            public ulong Dr1;
+            public ulong Dr2;
+            public ulong Dr3;
+            public ulong Dr6;
+            public ulong Dr7;
+
+            public ulong Rax;
+            public ulong Rcx;
+            public ulong Rdx;
+            public ulong Rbx;
+            public ulong Rsp;
+            public ulong Rbp;
+            public ulong Rsi;
+            public ulong Rdi;
+            public ulong R8;
+            public ulong R9;
+            public ulong R10;
+            public ulong R11;
+            public ulong R12;
+            public ulong R13;
+            public ulong R14;
+            public ulong R15;
+            public ulong Rip;
+
+            public XSAVE_FORMAT64 DUMMYUNIONNAME;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 26)]
+            public M128A[] VectorRegister;
+            public ulong VectorControl;
+
+            public ulong DebugControl;
+            public ulong LastBranchToRip;
+            public ulong LastBranchFromRip;
+            public ulong LastExceptionToRip;
+            public ulong LastExceptionFromRip;
+        }
+
+        #endregion
+
+        public static Process StartGame(Process game)
+        {
+            var si = new STARTUPINFO
+            {
+                cb = Marshal.SizeOf<STARTUPINFO>()
+            };
+
+            // I love win32
+            var env = string.Empty;
+            foreach (var v in game.StartInfo.Environment)
+            {
+                env += string.Join("=", v.Key, v.Value);
+                env += "\0";
+            }
+            env += "\0";
+
+            var envStr = Marshal.StringToHGlobalAnsi(env);
+
+            var commandLine = game.StartInfo.FileName + " " + game.StartInfo.Arguments;
+            if (!CreateProcess(game.StartInfo.FileName, commandLine, IntPtr.Zero, IntPtr.Zero, false, CreateProcessFlags.CREATE_SUSPENDED, envStr, game.StartInfo.WorkingDirectory, ref si, out PROCESS_INFORMATION pi))
+            {
+                Marshal.FreeHGlobal(envStr);
+                throw new Exception("CreateProcess failed: " + Marshal.GetLastWin32Error());
+            }
+            Marshal.FreeHGlobal(envStr);
+
+            game = Process.GetProcessById(pi.dwProcessId);
+
+            var ctx = new CONTEXT64();
+            ctx.ContextFlags = CONTEXT_FLAGS.CONTEXT_FULL;
+            var w = GetThreadContext(pi.hThread, ref ctx);
+
+            var asm = new byte[6]
+            {
+                    0x48, 0x33, 0xC0,   // XOR RAX, RAX
+                    0x90, 0x90, 0x90    // 3x NOP
+            };
+
+            var openProcessBytes = (IntPtr)
+            (
+                ctx.Rcx         // base module address
+                - 0x128fa04     // offset of entry point
+                + 0x57500       // offset of WinMain
+                + 0xb2          // offset of OpenProcess() call in WinMain
+            );
+
+            var worked = WriteProcessMemory(pi.hProcess, openProcessBytes, asm, 6, out IntPtr bytesWritten);
+            if (!worked)
+            {
+                var err = Marshal.GetLastWin32Error();
+            }
+
+            ResumeThread(pi.hThread);
+
+            CloseHandle(pi.hThread);
+            CloseHandle(pi.hProcess);
+
+            return Process.GetProcessById(pi.dwProcessId);
+        }
+    }
+}
