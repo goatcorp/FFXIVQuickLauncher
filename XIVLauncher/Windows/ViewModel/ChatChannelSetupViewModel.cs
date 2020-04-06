a1555482d42c9bd86a90fcc2c89fb97cb66c061c
goat <fakecake@icloud.com>
2020-04-06 22:11:36 +0900

    localization: window title for channel setup

--- XIVLauncher/Windows/ViewModel/ChatChannelSetupViewModel.cs
+++ XIVLauncher/Windows/ViewModel/ChatChannelSetupViewModel.cs
@@ -16,12 +16,14 @@ namespace XIVLauncher.Windows.ViewModel
 
         private void SetupLoc()
         {
+            ChatChannelSetupTitleLoc = Loc.Localize("ChatChannelSetupTitle", "Configure chat channel");
             ChatChannelSetupDescriptionLoc = Loc.Localize("ChatChannelSetupDescription",
                 "Please enter if XIVLauncher should post to a channel in a server or to an user in DMs,\r\nand their respective IDs.");
             HowLoc = Loc.Localize("HowToHint", "How do I set this up?");
             OkLoc = Loc.Localize("OK", "OK");
         }
 
+        public string ChatChannelSetupTitleLoc { get; private set; }
         public string ChatChannelSetupDescriptionLoc { get; private set; }
         public string HowLoc { get; private set; }
         public string OkLoc { get; private set; }
