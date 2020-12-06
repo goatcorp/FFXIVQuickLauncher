3ca39f14e584fa919a5af723d121e31b7c73ff95
goat <16760685+goaaats@users.noreply.github.com>
2020-12-06 11:57:39 +0100

    build: v5.2.0.0

--- XIVLauncher/Properties/AssemblyInfo.cs
+++ XIVLauncher/Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.2.0")]
-[assembly: AssemblyFileVersion("5.1.2.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Squirrel-Awareness
