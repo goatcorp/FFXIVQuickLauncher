958a19b599387fcbc65a3b3ab2caf1681213fb22
Aireil <33433913+Aireil@users.noreply.github.com>
2020-12-06 12:11:09 +0100

    Remove legacy settings

--- XIVLauncher/Properties/Settings.Designer.cs
+++ XIVLauncher/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace XIVLauncher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -260,18 +260,6 @@ namespace XIVLauncher.Properties {
             }
             set {
                 this["HasComplainedAboutAdmin"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
-        public int LauncherLanguage {
-            get {
-                return ((int)(this["LauncherLanguage"]));
-            }
-            set {
-                this["LauncherLanguage"] = value;
             }
         }
     }
