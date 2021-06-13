269189ccf5fcb96d5ed023d9c2d9d695b4ad267a
goat <16760685+goaaats@users.noreply.github.com>
2021-06-13 22:09:27 +0200

    feat: add IPatchAcquisition, AcquisitionProgress

---
+++ XIVLauncher/Game/Patch/Acquisition/AcquisitionProgress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVLauncher.Game.Patch.Acquisition
+{
+    public class AcquisitionProgress
+    {
+        public double Progress { get; set; }
+        public long BytesPerSecondSpeed { get; set; }
+    }
+}
---
+++ XIVLauncher/Game/Patch/Acquisition/IPatchAcquisition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using XIVLauncher.Game.Patch.PatchList;
+
+namespace XIVLauncher.Game.Patch.Acquisition
+{
+    interface IPatchAcquisition
+    {
+        public Task StartDownloadAsync(PatchListEntry patch, DirectoryInfo patchStore);
+
+        public event EventHandler<AcquisitionProgress> ProgressChanged;
+        public event EventHandler Complete;
+    }
+}
