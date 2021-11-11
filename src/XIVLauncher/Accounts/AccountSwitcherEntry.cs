b3363408aed7fc9fab77cb5f205cbe8ac0249ea9
goaaats <16760685+goaaats@users.noreply.github.com>
2021-11-11 22:24:38 +0100

    feat: cache account switcher entries

--- src/XIVLauncher/Accounts/AccountSwitcherEntry.cs
+++ src/XIVLauncher/Accounts/AccountSwitcherEntry.cs
@@ -14,28 +14,43 @@ namespace XIVLauncher.Accounts
     {
         public XivAccount Account { get; set; }
         public ImageSource ProfileImage { get; set; } = new BitmapImage(new Uri("pack://application:,,,/Resources/defaultprofile.png", UriKind.Absolute));
-        
+
         public void UpdateProfileImage()
         {
             if (string.IsNullOrEmpty(Account.ThumbnailUrl))
                 return;
 
-            using (var client = new WebClient())
+            var cacheFolder = Path.Combine(Paths.RoamingPath, "profilePictures");
+            Directory.CreateDirectory(cacheFolder);
+
+            var uri = new Uri(Account.ThumbnailUrl);
+            var cacheFile = Path.Combine(cacheFolder, uri.Segments.Last());
+
+            byte[] imageBytes;
+
+            if (File.Exists(cacheFile))
             {
-                var imageBytes = client.DownloadData(Account.ThumbnailUrl);
-
-                using (var stream = new MemoryStream(imageBytes))
-                {
-                    var bitmapImage = new BitmapImage();
-                    bitmapImage.BeginInit();
-                    bitmapImage.StreamSource = stream;
-                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmapImage.EndInit();
-                    bitmapImage.Freeze();
-
-                    ProfileImage = bitmapImage;
-                }
+                imageBytes = File.ReadAllBytes(cacheFile);
             }
+            else
+            {
+                using (var client = new WebClient())
+                {
+                    imageBytes = client.DownloadData(uri);
+                }
+
+                File.WriteAllBytes(cacheFile, imageBytes);
+            }
+
+            using var stream = new MemoryStream(imageBytes);
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = stream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+
+            ProfileImage = bitmapImage;
         }
     }
 }
