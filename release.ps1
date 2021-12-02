cae2bfb62e83879399a8b2d9e269c4c28f728cef
goaaats <16760685+goaaats@users.noreply.github.com>
2021-12-02 19:22:20 +0100

    add checklist to release script

--- release.ps1
+++ release.ps1
@@ -1,3 +1,11 @@
+Write-Output "Did you check..."
+Write-Output "1) That you are on master?"
+Write-Output "2) That you are up to date with origin?"
+Write-Output "3) That the changelog is updated?"
+Write-Output "4) That the assembly version is updated?"
+Write-Output "5) That you specified the correct assembly version when running this script?"
+Read-Host
+
 $version = $args[0]
 
 git add .
