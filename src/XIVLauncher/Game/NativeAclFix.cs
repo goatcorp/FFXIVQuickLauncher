717fac130f109dc744c4bcf3fa495a1bde8fc2df
goaaats <16760685+goaaats@users.noreply.github.com>
2021-12-09 19:52:11 +0100

    feat: set env var when staging

--- src/XIVLauncher/Game/NativeAclFix.cs
+++ src/XIVLauncher/Game/NativeAclFix.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Reflection;
 using Microsoft.Win32.SafeHandles;
 using System.Threading;
+using XIVLauncher.Settings;
 
 namespace XIVLauncher.Game
 {
@@ -375,6 +376,9 @@ namespace XIVLauncher.Game
                 {
                     cb = Marshal.SizeOf<PInvoke.STARTUPINFO>()
                 };
+
+                if (DalamudSettings.GetSettings().DoDalamudTest)
+                    Environment.SetEnvironmentVariable("DALAMUD_IS_STAGING", "true");
 
                 var compatLayerPrev = Environment.GetEnvironmentVariable("__COMPAT_LAYER");
                 Environment.SetEnvironmentVariable("__COMPAT_LAYER", "RunAsInvoker");
