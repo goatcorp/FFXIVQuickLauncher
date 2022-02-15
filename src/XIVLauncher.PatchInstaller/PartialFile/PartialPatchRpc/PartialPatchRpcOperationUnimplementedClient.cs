22f302ce5a2f4b3ef9754bb8daa8dce3921af9ea
Soreepeong <soreepeong@gmail.com>
2022-02-15 18:32:59 +0900

    Add example unimplemented client

---
+++ src/XIVLauncher.PatchInstaller/PartialFile/PartialPatchRpc/PartialPatchRpcOperationUnimplementedClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace XIVLauncher.PatchInstaller.PartialFile.PartialPatchRpc
+{
+    class PartialPatchRpcOperationUnimplementedClient : PartialPatchRpcOperationClient
+    {
+        public PartialPatchRpcOperationUnimplementedClient(string rpcChannelName)
+            : base(rpcChannelName)
+        {
+        }
+
+        public override void Run()
+        {
+            // TODO: load patch index files and provide those to server
+            throw new NotImplementedException();
+
+            ProvideIndexFile(@"C:\Game Path\boot", "ffxivboot", new FileStream("D2021.11.16.0000.0001.patch.index", FileMode.Open, FileAccess.Read));
+            ProvideIndexFile(@"C:\Game Path\game", "ffxivgame", new FileStream("D2022.01.25.0000.0000.patch.index", FileMode.Open, FileAccess.Read));
+            ProvideIndexFileFinish();
+
+            base.Run();
+        }
+
+        protected override void OnRequestPartialFile(int patchSetIndex, short patchFileIndex, string patchFileName, List<RequestedPartInfo> parts)
+        {
+            // TODO: download `patchFileName` for the parts specified in `parts`
+            throw new NotImplementedException();
+
+            ProvidePartialFile(patchSetIndex, patchFileIndex, $@"C:\Temp\{patchFileName}");
+        }
+
+        protected override void OnStatusUpdate(float progress, long applyProgress, long applyProgressMax)
+        {
+            // TODO: show progress
+            throw new NotImplementedException();
+        }
+
+        protected override void OnFinishPartialFile(int patchSetIndex, short patchFileIndex, string patchFileName)
+        {
+            // TODO: maybe delete temporary patch file
+            throw new NotImplementedException();
+        }
+    }
+}
