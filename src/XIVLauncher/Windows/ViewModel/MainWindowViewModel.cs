91046b561c4fd409877821333a429addf20f6c53
goaaats <goatsdev@protonmail.com>
2022-03-08 03:16:35 +0100

    build: 6.2.5

--- src/XIVLauncher/Windows/ViewModel/MainWindowViewModel.cs
+++ src/XIVLauncher/Windows/ViewModel/MainWindowViewModel.cs
@@ -138,7 +138,7 @@ namespace XIVLauncher.Windows.ViewModel
             var bootver = SeVersion.Parse(Repository.Boot.GetVer(App.Settings.GamePath));
             var ver600 = SeVersion.Parse("2021.11.16.0000.0001");
 
-            if (bootver > ver600)
+            if (bootver > ver600 || true)
             {
                 CustomMessageBox.Show(Loc.Localize("KillswitchText", "XIVLauncher cannot start the game at this time, as Square Enix has made changes to the login process." +
                                                                      "\nWe need to adjust to these changes and verify that our adjustments are safe before we can re-enable the launcher. Please try again later." +
@@ -146,6 +146,7 @@ namespace XIVLauncher.Windows.ViewModel
                                                                      "\n") + Loc.Localize("SteamLinkingText", "You may be prompted to link your Steam account to your Square Enix account.")
                     , "XIVLauncher", MessageBoxButton.OK, MessageBoxImage.None, showHelpLinks: false, showDiscordLink: true, showOfficialLauncher: true);
 
+                Environment.Exit(0);
                 return;
             }
             /* =================================================== */
