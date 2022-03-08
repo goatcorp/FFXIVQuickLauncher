ad9e2210ef2652952ff59418d5ee56a8144b7ede
goaaats <goatsdev@protonmail.com>
2022-03-08 16:42:52 +0100

    feat: MSVC CRT rand implementation

---
+++ src/XIVLauncher.Common/Encryption/CrtRand.cs
@@ -0,0 +1,17 @@
+﻿namespace XIVLauncher.Common.Encryption;
+
+public class CrtRand
+{
+    private uint seed;
+
+    public CrtRand(uint seed)
+    {
+        this.seed = seed;
+    }
+
+    public uint Next()
+    {
+        this.seed = 0x343FD * this.seed + 0x269EC3;
+        return ((this.seed >> 16) & 0xFFFF) & 0x7FFF;
+    }
+}
