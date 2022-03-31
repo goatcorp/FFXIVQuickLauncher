e13880155186bf521f1b60b93caecb0331e5bd28
goaaats <goatsdev@protonmail.com>
2022-03-31 12:58:54 +0200

    feat: NumericSettingsEntry with steps, patch speed selector

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabPatching.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabPatching.cs
@@ -6,7 +6,7 @@ public class SettingsTabPatching : SettingsTab
 {
     public override SettingsEntry[] Entries { get; } =
     {
-        new SettingsEntry<DirectoryInfo>("Patch Path", "Where patches should be downloaded to.", () => Program.Config.GamePath, x => Program.Config.GamePath = x)
+        new SettingsEntry<DirectoryInfo>("Patch Path", "Where patches should be downloaded to.", () => Program.Config.PatchPath, x => Program.Config.PatchPath = x)
         {
             CheckValidity = x =>
             {
@@ -19,8 +19,8 @@ public class SettingsTabPatching : SettingsTab
 
         new SettingsEntry<AcquisitionMethod>("Patch Download Method", "How patches should be downloaded.", () => Program.Config.PatchAcquisitionMethod ?? AcquisitionMethod.Aria,
             x => Program.Config.PatchAcquisitionMethod = x),
-        new NumericSettingsEntry<long>("Maximum Speed", "Maximum download speed in bytes per second. Set to 0 for unlimited.", () => Program.Config.PatchSpeedLimit,
-            x => Program.Config.PatchSpeedLimit = x, 0, long.MaxValue, 1000),
+        new NumericSettingsEntry("Maximum Speed", "Maximum download speed in bytes per second. Set to 0 for unlimited.", () => (int)Program.Config.PatchSpeedLimit,
+            x => Program.Config.PatchSpeedLimit = x, 0, int.MaxValue, 1000),
         new SettingsEntry<bool>("Keep Patches", "Keep patches on disk after installing.", () => Program.Config.KeepPatches ?? false, x => Program.Config.KeepPatches = x),
     };
 
