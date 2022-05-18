4863ffc3db32eb1270eb74f54abf908045f4ba25
marzent <marc_aurel@me.com>
2022-05-18 08:06:19 +0200

    handle spaces in path correctly for non-dalamud launches on Unix systems

--- src/XIVLauncher.Common.Unix/UnixGameRunner.cs
+++ src/XIVLauncher.Common.Unix/UnixGameRunner.cs
@@ -33,7 +33,7 @@ public class UnixGameRunner : IGameRunner
         }
         else
         {
-            return compatibility.RunInPrefix(path + " " + arguments, workingDirectory, environment);
+            return compatibility.RunInPrefix($"\"{path}\" {arguments}", workingDirectory, environment);
         }
     }
 }
