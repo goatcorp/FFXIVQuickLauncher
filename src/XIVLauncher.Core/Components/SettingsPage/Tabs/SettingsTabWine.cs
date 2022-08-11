4b951c446320b5c8569f668a5b713c5b0b48d5c8
Oro <93224879+orowith2os@users.noreply.github.com>
2022-08-11 13:37:41 -0500

    Checks for the files without checking for if running in a Flatpak

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabWine.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabWine.cs
@@ -29,9 +29,7 @@ public class SettingsTabWine : SettingsTab
                 CheckValidity = b =>
                 {
 
-                     if (b == true && !FLATPAK && !File.Exists("/usr/lib/libgamemodeauto.so.0"))
-                         return "GameMode not detected.";
-                     else if (b == true && FLATPAK && !File.Exists("/app/lib/libgamemodeauto.so.0"))
+                     if (b == true && (!File.Exists("/usr/lib/libgamemodeauto.so.0") && !File.Exists("/app/lib/libgamemodeauto.so.0")))
                          return "GameMode not detected.";
 
                     return null;
