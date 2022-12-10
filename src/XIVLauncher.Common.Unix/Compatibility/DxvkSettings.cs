84ed99999a1404754581913014929c62d2fb1b18
Rankyn Bass <rankyn@proton.me>
2022-12-10 12:45:14 -0800

    fix: Make sure to create .dxvk folder

--- src/XIVLauncher.Common.Unix/Compatibility/DxvkSettings.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/DxvkSettings.cs
@@ -28,6 +28,8 @@ public class DxvkSettings
         string home = Environment.GetEnvironmentVariable("HOME");
         corePath ??= Path.Combine(home,".xlcore");
         string dxvkConfigPath = Path.Combine(corePath,".dxvk");
+        if (!Directory.Exists(dxvkConfigPath))
+            Directory.Create(dxvkConfigPath);
         this.DxvkVars = new Dictionary<string, string> ();
         this.DxvkVars.Add("DXVK_LOG_PATH",Path.Combine(corePath,"logs"));
         this.DxvkVars.Add("DXVK_CONFIG_FILE",Path.Combine(dxvkConfigPath,"dxvk.conf"));
