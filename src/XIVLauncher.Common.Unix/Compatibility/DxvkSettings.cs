c138a3aa7b262c842a96bde76ed03bae800cfbfb
Rankyn Bass <rankyn@proton.me>
2022-12-10 15:44:15 -0800

    fix: use DirectoryInfo for xlcore path

--- src/XIVLauncher.Common.Unix/Compatibility/DxvkSettings.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/DxvkSettings.cs
@@ -22,17 +22,17 @@ public class DxvkSettings
 
     public DxvkSettings(Dxvk.DxvkHudType hud = Dxvk.DxvkHudType.None, string dxvkHudCustom="",
         string mangoHudPath="", bool? async = true, int? frameRate = 0,
-        Dxvk.DxvkVersion version = Dxvk.DxvkVersion.v1_10_1, string? corePath = null)
+        Dxvk.DxvkVersion version = Dxvk.DxvkVersion.v1_10_1, DirectoryInfo? corePath = null)
     {
         this.DxvkHud = hud;
         string home = Environment.GetEnvironmentVariable("HOME");
-        corePath ??= Path.Combine(home,".xlcore");
-        string dxvkConfigPath = Path.Combine(corePath,".dxvk");
-        if (!Directory.Exists(dxvkConfigPath))
-            Directory.CreateDirectory(dxvkConfigPath);
+        corePath ??= new DirectoryInfo(Path.Combine(home,".xlcore"));
+        var dxvkConfigPath = new DirectoryInfo(Path.Combine(corePath.FullName,".dxvk"));
+        if (!dxvkConfigPath.Exists)
+            dxvkConfigPath.Create();
         this.DxvkVars = new Dictionary<string, string> ();
-        this.DxvkVars.Add("DXVK_LOG_PATH",Path.Combine(corePath,"logs"));
-        this.DxvkVars.Add("DXVK_CONFIG_FILE",Path.Combine(dxvkConfigPath,"dxvk.conf"));
+        this.DxvkVars.Add("DXVK_LOG_PATH",Path.Combine(corePath.FullName,"logs"));
+        this.DxvkVars.Add("DXVK_CONFIG_FILE",Path.Combine(dxvkConfigPath.FullName,"dxvk.conf"));
         this.DxvkVars.Add("DXVK_ASYNC", ((async ?? false) ? "1" : "0"));
         frameRate ??= 0;
         if (frameRate > 0) this.DxvkVars.Add("DXVK_FRAME_RATE", (frameRate).ToString());
@@ -47,8 +47,8 @@ public class DxvkSettings
         };
         this.DownloadURL = $"https://github.com/Sporif/dxvk-async/releases/download/{release}/dxvk-async-{release}.tar.gz";
         this.FolderName = $"dxvk-async-{release}";
-        string dxvkCacheSync = release + ((async ?? false) ? "-async" : ""); 
-        this.DxvkVars.Add("DXVK_STATE_CACHE_PATH",Path.Combine(dxvkConfigPath,dxvkCacheSync));
+        string dxvkCachePath = Path.Combine(dxvkConfigPath.FullName, release + ((async ?? false) ? "-async" : "")); 
+        this.DxvkVars.Add("DXVK_STATE_CACHE_PATH", dxvkCachePath);
         switch(this.DxvkHud)
         {
             case Dxvk.DxvkHudType.Fps:
@@ -74,7 +74,7 @@ public class DxvkSettings
                 DxvkVars.Add("MANGOHUD","1");
                 if (mangoHudPath == "")
                 {
-                    string conf1 = Path.Combine(corePath,"MangoHud.conf");
+                    string conf1 = Path.Combine(corePath.FullName,"MangoHud.conf");
                     string conf2 = Path.Combine(home,".config","MangoHud","wine-ffxiv_dx11.conf");
                     string conf3 = Path.Combine(home,".config","MangoHud","MangoHud.conf");
                     if (CheckMangoHudPath(conf1))
