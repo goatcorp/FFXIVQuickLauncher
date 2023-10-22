47ef20566a7de021114aae109d04fd798cdf2980
Soreepeong <soreepeong@gmail.com>
2023-10-22 16:55:47 +0900

    Make PatchInstaller use System.CommandLine

---
+++ src/XIVLauncher.PatchInstaller/Commands/IndexVerifyCommand.cs
@@ -0,0 +1,65 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using XIVLauncher.Common.Patching.IndexedZiPatch;
+
+namespace XIVLauncher.PatchInstaller.Commands;
+
+public class IndexVerifyCommand
+{
+    public static readonly Command COMMAND = new("index-verify", "Verify and optionally repair a game installation.");
+
+    private static readonly Argument<string> PatchIndexFileArgument = new("patch-index-file", "Path to a patch index file. (*.patch.index)");
+
+    private static readonly Argument<string> GameRootPathArgument = new(
+        "game-path",
+        "Root folder of a game installation, such as \"C:\\Program Files (x86)\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn\\\"");
+
+    private static readonly Argument<string?> PatchRootPathArgument = new("patch-root-path", "Path to a folder containing relevant patch files.")
+    {
+        Arity = ArgumentArity.ZeroOrOne,
+    };
+
+    private static readonly Option<int> ThreadCountOption = new(
+        new[] { "-t", "--threads" },
+        () => Math.Min(Environment.ProcessorCount, 8),
+        "Number of threads. Specifying 0 will use all available cores.");
+
+    static IndexVerifyCommand()
+    {
+        COMMAND.AddAlias("index-repair");
+        COMMAND.AddArgument(PatchIndexFileArgument);
+        COMMAND.AddArgument(GameRootPathArgument);
+        COMMAND.AddArgument(PatchRootPathArgument);
+        ThreadCountOption.AddValidator(x => x.ErrorMessage = x.GetValueOrDefault<int>() >= 0 ? null : "Must be 0 or more");
+        COMMAND.AddOption(ThreadCountOption);
+        COMMAND.SetHandler(x => new IndexVerifyCommand(x.ParseResult).Handle());
+    }
+
+    private readonly string patchIndexFile;
+    private readonly string gameRootPath;
+    private readonly string? patchRootPath;
+    private readonly int threadCount;
+
+    private IndexVerifyCommand(ParseResult parseResult)
+    {
+        this.patchIndexFile = parseResult.GetValueForArgument(PatchIndexFileArgument);
+        this.gameRootPath = parseResult.GetValueForArgument(GameRootPathArgument);
+        this.patchRootPath = parseResult.GetValueForArgument(PatchRootPathArgument);
+        this.threadCount = parseResult.GetValueForOption(ThreadCountOption);
+        if (this.threadCount == 0)
+            this.threadCount = Environment.ProcessorCount;
+        Debug.Assert(this.threadCount > 0);
+    }
+
+    private async Task<int> Handle()
+    {
+        if (this.patchRootPath is null)
+            await IndexedZiPatchOperations.VerifyFromZiPatchIndex(this.patchIndexFile, this.gameRootPath);
+        else
+            await IndexedZiPatchOperations.RepairFromPatchFileIndexFromFile(this.patchIndexFile, this.gameRootPath, this.patchRootPath, this.threadCount);
+        return 0;
+    }
+}
