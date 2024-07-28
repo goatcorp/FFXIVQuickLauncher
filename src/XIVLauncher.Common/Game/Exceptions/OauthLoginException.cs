a752c2ed597b67c7db8d0b85e268eca0d4c3402b
Rankyn Bass <rankyn@proton.me>
2024-07-28 12:25:31 -0700

    Add logic to catch XL.Core login exceptions properly

--- src/XIVLauncher.Common/Game/Exceptions/OauthLoginException.cs
+++ src/XIVLauncher.Common/Game/Exceptions/OauthLoginException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Linq;
 using Serilog;
 
 namespace XIVLauncher.Common.Game.Exceptions;
@@ -9,6 +10,11 @@ public class OauthLoginException : Exception
 {
     private static Regex errorMessageRegex =
         new(@"window.external.user\(""login=auth,ng,err,(?<errorMessage>.*)\""\);", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    // XL.Core error messages used in MainPage.cs. Needs to match for this to work.
+    private static string[] xlcoreMessages = 
+        new string[] { "No service account or subscription", "Need to accept terms of use", 
+                       "Boot conflict, need reinstall", "Repair login state not NeedsPatchGame" };
 
     public string? OauthErrorMessage { get; private set; }
 
@@ -21,6 +27,10 @@ public class OauthLoginException : Exception
     private static string? GetErrorMessage(string document)
     {
         var matches = errorMessageRegex.Matches(document);
+
+        // Handle xlcore error messages
+        if (xlcoreMessages.Contains(document))
+            return document;
 
         if (matches.Count is 0 or > 1)
         {
