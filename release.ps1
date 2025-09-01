188aed6bdcc247ba59ff0866938b177e3bac40c8
goaaats <goatsdev@protonmail.com>
2025-09-01 21:28:37 +0200

    build: 7.0.0

--- release.ps1
+++ release.ps1
@@ -10,5 +10,5 @@ $version = $args[0]
 
 git add .
 git commit -m "build: ${version}"
-git tag -a -s -m "This is XIVLauncher build ${version}" "${version}"
+git tag -a -m "This is XIVLauncher build ${version}" "${version}"
 git push --atomic origin master ${version}
